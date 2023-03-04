cac50ad10c872df47b4718df3aa710efb8c616dd
Hnatiak <romanhnatiak@gmail.com>
2023-03-04 15:27:34 +0100

    goit-react-hw-01-components

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Section from "./components/Section/Section";
+import Description from './components/Profile/Description/Description';
+import Statistics from './components/Statistics/Statistics';
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import user from "./data/user.json";
+import data from './data/data.json';
+import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
+
+export function App() {
+  return ( 
+  <div>
+      <Section title={"Task-1 'Social network profile'"}>
+        <Description
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Section>
+
+      <Section title={"Task-2 'Statistics section'"}>
+        <Statistics title="Statics" stats={data} />
+      </Section>
+
+      <Section title={"Task-3 'Friend list'"}>
+        <FriendList friends={friends} />
+      </Section>
+
+      <Section title={"Task-3 'Transactions'"}>
+        <TransactionHistory transactions={transactions} />
+      </Section>
+
+
+    </div> 
+    );
+}
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import FriendListItem from './FriendListItem/FriendListItem'
+
+export default function FriendList({friends}) {
+    return (
+        <ul>
+        {friends.map(({ id, isOnline, avatar, name }) => (
+          <FriendListItem
+            key={id}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        ))}
+      </ul>
+  )}
+
+  FriendListItem.prototype = {
+    friends: PropTypes.arrayOf(
+      PropTypes.shape({
+        isOnline: PropTypes.bool,
+        avatar: PropTypes.string,
+        name: PropTypes.string,
+      })
+    ).isRequired,
+  };
